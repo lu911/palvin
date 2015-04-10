b54d423c936f29fa5ee2a56c6a9a5cfbc09a7be2
Loup <loup1788@gmail.com>
2015-04-10 16:02:44 +0900

    All models auto imported in shell.

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,19 @@
 # -*- coding:utf-8 -*-
+import os
+
+for root, dirnames, filenames in os.walk('palvin/models'):
+    for filename in filenames:
+        split_filename = filename.split('.')
+        if split_filename[-1] == 'py' and filename != '__init__.py':
+            modname = '%s.%s' % (root.replace('/', '.'), split_filename[0])
+            print "* Import %s..." % modname,
+            try:
+                module = __import__(
+                    modname, globals=globals(), locals=locals(), fromlist=['*']
+                )
+            except ImportError:
+                continue
+            else:
+                for object_ in dir(module):
+                    locals()[object_] = getattr(module, object_)
+                print 'Done'
