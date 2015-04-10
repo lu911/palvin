8649c07eb7fea8ade2787be7f5a2dbcf2dda439d
Loup <loup1788@gmail.com>
2015-04-10 16:11:19 +0900

    minor naming changed.

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,14 @@
 # -*- coding:utf-8 -*-
 import os
 
-for root, dirnames, filenames in os.walk('palvin/models'):
-    for filename in filenames:
+for root, directory_names, file_names in os.walk('palvin/models'):
+    for filename in file_names:
         split_filename = filename.split('.')
         if split_filename[-1] == 'py' and filename != '__init__.py':
             modname = '%s.%s' % (root.replace('/', '.'), split_filename[0])
             print "* Import %s..." % modname,
             try:
-                module = __import__(
-                    modname, globals=globals(), locals=locals(), fromlist=['*']
-                )
+                module = __import__(modname, globals=globals(), locals=locals(), fromlist=['*'])
             except ImportError:
                 continue
             else:
