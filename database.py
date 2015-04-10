ae52a7936889eebf8f1f8276f99f4152341800b3
Loup <loup1788@gmail.com>
2015-04-10 12:16:21 +0900

    database.py comment minor changed.

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-    This module provides configurations. Secret data is stored in `secret` module.
+    Secret data is stored in `secret` module.
     :secret.DATABASE_USERNAME: A username for database
     :secret.DATABASE_PASSWORD: A password for database.
     :secret.DATABASE: A name for database.
