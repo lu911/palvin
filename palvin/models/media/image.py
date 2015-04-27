f3a2b7336e9472ebb09267857c8aa567bec8af75
Loup <loup1788@gmail.com>
2015-04-27 09:18:17 -0700

    Basic image models orm error fixed.

--- palvin/models/media/image.py
+++ palvin/models/media/image.py
@@ -1,26 +1,32 @@
 # -*- coding:utf-8 -*-
 from sqlalchemy import Column, Enum, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declared_attr
 
-from palvin.database import IdMixin, CRUDMixin, Base
+from palvin.database import IdMixin, Base
 from palvin.models.media.constants import IMAGE_SIZE_TYPES
 
 
-class ImageType(IdMixin, CRUDMixin, Base):
+class ImageType(IdMixin, Base):
     name = Column(String(20), nullable=False, index=True)
-    parent_id = Column(
-        Integer,
-        ForeignKey('%s.id' % Base.__tablename__, ondelete='CASCADE')
-    )
-    parent = relationship(
-        'ImageType',
-        remote_side='ImageType.id',
-        backref=backref('children', cascade='all, delete, delete-orphan'),
-        cascade='all'
-    )
+
+    @declared_attr
+    def parent_id(cls):
+        return Column(
+            Integer,
+            ForeignKey('%s.id' % cls.__tablename__, ondelete='CASCADE')
+        )
+
+    @declared_attr
+    def parent(cls):
+        return relationship(
+            cls.__name__,
+            remote_side='%s.id' % cls.__name__,
+            backref=backref('children', cascade='all, delete, delete-orphan')
+        )
 
 
-class Image(IdMixin, CRUDMixin, Base):
+class Image(IdMixin, Base):
 
     def __init__(self):
         pass
@@ -34,13 +40,13 @@ class Image(IdMixin, CRUDMixin, Base):
         pass
 
 
-class ImageSize(IdMixin, CRUDMixin, Base):
+class ImageSize(IdMixin, Base):
     size = Column(
-        Enum(*IMAGE_SIZE_TYPES.keys(), name='size'),
+        Enum(*IMAGE_SIZE_TYPES, name='size'),
         nullable=False,
         index=True
     )
-    filename = Column(String(255), nullable=False)
+    filename = Column(String(255), nullable=False, index=True)
     width = Column(Integer, nullable=False)
     height = Column(Integer, nullable=False)
     image_id = Column(Integer, ForeignKey('%s.id' % Image.__tablename__))
