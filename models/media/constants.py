76f3060eee4fe140278168ee1e645e29c309d52d
Loup <loup1788@gmail.com>
2015-04-27 08:21:54 -0700

    Basic image models added.

---
+++ models/media/constants.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+# Photo Sizes
+IMAGE_SIZE = {
+    'THUMBNAIL': (50, 50),
+    'SMALL': (120, 80),
+    'MEDIUM': (240, 160),
+    'LARGE': (360, 240),
+}
+IMAGE_SIZE_TYPES = IMAGE_SIZE.keys()
---
+++ models/media/image.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+from database import IdMixin, CRUDMixin, Base
+from models.media.constants import IMAGE_SIZE_TYPES
+
+from sqlalchemy import Column, Enum, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+
+class ImageType(IdMixin, CRUDMixin, Base):
+    name = Column(String(20), nullable=False, index=True)
+    parent_id = Column(
+        Integer,
+        ForeignKey('%s.id' % Base.__tablename__, ondelete='CASCADE')
+    )
+    parent = relationship(
+        'ImageType',
+        remote_side='ImageType.id',
+        backref=backref('children', cascade='all, delete, delete-orphan'),
+        cascade='all'
+    )
+
+
+class Image(IdMixin, CRUDMixin, Base):
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_image_object(cls, file_):
+        pass
+
+    @classmethod
+    def from_image_url(cls, url):
+        pass
+
+
+class ImageSize(IdMixin, CRUDMixin, Base):
+    size = Column(
+        Enum(*IMAGE_SIZE_TYPES.keys(), name='size'),
+        nullable=False,
+        index=True
+    )
+    filename = Column(String(255), nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    image_id = Column(Integer, ForeignKey('%s.id' % Image.__tablename__))
